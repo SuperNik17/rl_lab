2d6c1da048b9c179d6f018714b6e8d0832f639ce
Armando <105849491+SuperNik17@users.noreply.github.com>
2023-03-12 14:08:36 +0100

    Update ex0_bash.sh
    Edited for privacy reason.

--- ex0_bash.sh
+++ ex0_bash.sh
@@ -1,9 +1,9 @@
 #! bin/bash
 
-touch nicolella.txt
-echo "Armando Nicolella" > nicolella.txt  
+touch n.txt
+echo "Armando N" > n.txt  
 mkdir -p rl_lab/ex0
-mv nicolella.txt ./rl_lab/ex0/
+mv n.txt ./rl_lab/ex0/
 mv ./rl_lab/ex0/ ./rl_lab/ex1
-grep -i "Nicolella" ./rl_lab/ex1/nicolella.txt 
-sudo chown root:root ./rl_lab/ex1/nicolella.txt 
+grep -i "N" ./rl_lab/ex1/n.txt 
+sudo chown root:root ./rl_lab/ex1/n.txt 
